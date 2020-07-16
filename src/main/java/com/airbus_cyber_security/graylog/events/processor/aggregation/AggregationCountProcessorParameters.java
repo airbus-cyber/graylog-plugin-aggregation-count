754422f70eacbc60d42f7476922dd0b6c61accc6
c8y3 <25362953+c8y3@users.noreply.github.com>
2020-07-16 09:19:13 +0200

    Moved AggregationCountProcessorParameters into namespace events.processor.aggregation to mimic graylog file structure

--- src/main/java/com/airbus_cyber_security/graylog/events/processor/aggregation/AggregationCountProcessorParameters.java
+++ src/main/java/com/airbus_cyber_security/graylog/events/processor/aggregation/AggregationCountProcessorParameters.java
@@ -1,6 +1,5 @@
-package com.airbus_cyber_security.graylog;
+package com.airbus_cyber_security.graylog.events.processor.aggregation;
 
-import com.airbus_cyber_security.graylog.events.processor.aggregation.AggregationCountProcessorConfig;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
