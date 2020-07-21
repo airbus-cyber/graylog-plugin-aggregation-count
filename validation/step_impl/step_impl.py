afc23180274907dfc0d5993f670671ce3d44b955
c8y3 <25362953+c8y3@users.noreply.github.com>
2020-07-21 10:17:31 +0200

    Started selenium firefox webdriver in tests

--- validation/step_impl/step_impl.py
+++ validation/step_impl/step_impl.py
@@ -1,42 +1,25 @@
-from getgauge.python import step
-from pathlib import Path
-import subprocess
-from threading import Thread
-from queue import Queue
+from getgauge.python import before_suite, after_suite, step
+from selenium.webdriver import Firefox
+from step_impl.graylog_server import GraylogServer
 
-def put_stream_lines_in_queue(stream, queue):
-    while True:
-        line = stream.readline()
-        if (line == ''): break
-        queue.put(line)
-
-def docker_compose(docker_compose_command):
-    command = ['docker-compose'] + docker_compose_command
-    subprocess.run(command, cwd='execution_environment')
-
+@before_suite
+def init():
+    # Note: I am not sure this is better, or using the data_store.suite is better...
+    global browser
+    global server
+    server = GraylogServer()
+    browser = Firefox()
+    
+@after_suite
+def close():
+    browser.close()
 
 @step("Start Graylog server")
 def start_graylog_server():
-    docker_compose(['up', '--detach'])
-    # TODO clean this up and factor with the other docker_compose commands
-    graylog_logs = subprocess.Popen(['docker-compose', 'logs', '--no-color', '--follow'], stdout=subprocess.PIPE, text=True, cwd='execution_environment')
-    logs = Queue()
-    reading_logs = Thread(target=put_stream_lines_in_queue, args=[graylog_logs.stdout, logs])
-    reading_logs.start()
-    while True:
-        try:
-            log = logs.get(1)
-        except Empty:
-            raise AssertionError('Graylog server did not start correctly')
-            break
-        print(log)
-        if 'Graylog server up and running.' in log:
-            break
-    graylog_logs.terminate()
-    reading_logs.join()
-
+    server.start()
+    server.wait_until_log('Graylog server did not start correctly')
     
 @step("Stop Graylog server")
 def stop_graylog_server():
-    docker_compose(['down'])
+    server.stop()
 
