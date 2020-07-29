b31cfcacb758b67ce5284226399dc9d906fb0307
c8y3 <25362953+c8y3@users.noreply.github.com>
2020-07-29 10:58:32 +0200

    Factored code to format the result

--- src/main/java/com/airbus_cyber_security/graylog/events/processor/aggregation/checks/Result.java
+++ src/main/java/com/airbus_cyber_security/graylog/events/processor/aggregation/checks/Result.java
@@ -2,15 +2,17 @@ package com.airbus_cyber_security.graylog.events.processor.aggregation.checks;
 
 import org.graylog2.plugin.MessageSummary;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Result {
 
-    private String resultDescription;
+    private final String resultDescription;
 
-    private List<MessageSummary> messageSummaries;
+    private final List<MessageSummary> messageSummaries;
 
-    public Result(final String resultDescription, final List<MessageSummary> messageSummaries) {
+    private Result(final String resultDescription, final List<MessageSummary> messageSummaries) {
         this.resultDescription = resultDescription;
         this.messageSummaries = messageSummaries;
     }
@@ -21,5 +23,22 @@ public class Result {
 
     public List<MessageSummary> getMessageSummaries() {
         return messageSummaries;
+    }
+
+    public static class Builder {
+        private final String resultDescriptionPattern;
+
+        public Builder(String resultDescriptionPattern) {
+            this.resultDescriptionPattern = resultDescriptionPattern;
+        }
+
+        public Result buildEmpty() {
+            return new Result("", new ArrayList<>());
+        }
+
+        public Result build(long count, List<MessageSummary> summaries) {
+            String resultDescription = MessageFormat.format(this.resultDescriptionPattern, count);
+            return new Result(resultDescription, summaries);
+        }
     }
 }
